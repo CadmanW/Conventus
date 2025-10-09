90a6f0a57b1d4b3a549a3235944a6465d5c0410e
Cadmium <133188056+CadmanW@users.noreply.github.com>
2025-10-09 12:42:43 -0500

    merged mariaDB into webserver.js
    gotta figure out logina nd session id's n stuff

--- NodeJS/webserver.js
+++ NodeJS/webserver.js
@@ -1,18 +1,30 @@
-//! WEBSERVER
 // node modules
 const fs = require("fs");
 const path = require("path");
 const http = require("http");
+const mariadb = require("mariadb");
 
-// iomport my other nodeJS files
-// Even though we don't explicitly use maraiDBpool here, it is used by mariaDBaddUser() so we save the connection as a global
-const mariaDB = require(path.resolve(__dirname, "mariadb/mariadb.js"));
-
+//! Functions
 function isSanitized(str) {
     const allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^*-_,.";
     return str.split("").every(c => allowedChars.includes(c));
 }
 
+
+
+//! MariaDB
+// Create the pool, which manages connections
+const pool = mariadb.createPool({
+    host: "localhost",
+    user: "root",
+    password: "Cadman!13",
+    database: "ConventusDB",
+    connectionLimit: 5
+});
+
+
+
+//! Webserver
 const IP = "127.0.0.1";
 const PORT = "8080";
 const mime_types = {
@@ -59,14 +71,30 @@ server.on("request", (request, response) => {
         // Use the data when all chunks are recieved
         request.on("end", async () => {
             try {
-                const jsonData = Object.fromEntries(new URLSearchParams(body));
+                const {email, password} = Object.fromEntries(new URLSearchParams(body));
 
-                if (isSanitized(jsonData.email) && isSanitized(jsonData.password)) {
-                    await mariaDB.addUser(jsonData.email, jsonData.password);
-                } else throw Error(`user input:\n  username:`);
+                if (isSanitized(email) && isSanitized(password)) {
+                    // Check to see if this is a signin or signup by checking if email already is in database or not
+                    if (await pool.query("SELECT email FROM users WHERE email=?", email).email) {
+                        // login
+                        if (password == await pool.query("SELECT password FROM users WHERE email=?", email).password) {
+                            const sessionID = Math.random() * 10**15;  
+
+                            await pool.query("UPDATE users SET session_id=? WHERE email", sessionID);
+                        }
+                    }
+                    else { // add the user
+                        await pool.query("INSERT INTO users (email, password) VALUES (?, ?);", [email, password])
+                    }
+
+                } else {
+                    throw Error(`user input:\n  username:`);
+                }
             } catch (e) {
                 console.log("POST request error:\n" + e);
             }
+
+            response.end();
         });
     }
 });
